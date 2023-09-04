1b8d36c743e0a02e6284be4355b12275595ec2b1
obafisayo-alx <sekaniisjoy@gmail.com>
2023-09-04 09:25:56 +0100

    add new files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,166 @@
 import React from "react";
 import styled from "styled-components"
 import PiggyvestLogo from "../assets/piggyvest_logo.svg"
+import Compliance from "../assets/compliance.webp"
+import facebook from "../assets/facebook.svg"
+import twitter from "../assets/twitter.svg"
+import instagram from "../assets/instagram.svg"
+import tiktok from "../assets/tiktok.svg"
+import youtube from "../assets/youtube.svg"
 import { Link } from "react-router-dom";
 
 const StyledFooter = styled.footer`
-    padding: 80px, 16px, 50px;
+    padding: 80px 16px 50px;
     font-size: 12.82px;
     background: #fff;
+    .container {
+        max-width: 1165px;
+        margin: auto;
+    }
+    .row {
+        display: -webkit-flex;
+        display: -moz-box;
+        display: flex;
+        -webkit-flex-wrap: wrap;
+        flex-wrap: wrap;
+        margin-right: -15px;
+        margin-left: -15px;
+    }
+    .about {
+        margin: 16px 0 30px;
+        border-top: 0.2px solid hsla(0,0%,74.1%,.5215686275);
+        padding-top: 26px;
+    }
+    [class*=col-] {
+        margin-bottom: 30px;
+    }
+    .img-container {
+        display: -webkit-flex;
+        display: -moz-box;
+        display: flex;
+        -webkit-flex-direction: column;
+        -moz-box-orient: vertical;
+        -moz-box-direction: normal;
+        flex-direction: column;
+        max-width: 173px;
+        -webkit-align-items: center;
+        -moz-box-align: center;
+        align-items: center;
+    }
+    .logo {
+        display: -webkit-inline-flex;
+        display: -moz-inline-box;
+        display: inline-flex;
+    }
+    .mb-25 {
+        margin-bottom: 25px;
+    }
+    a {
+        display: block;
+        text-align: left;
+        font-weight: 300!important;
+        -webkit-transition: color .5s;
+        -moz-transition: color .5s;
+        transition: color .5s;
+    }
+    .socials-cont p, a {
+        color: #3d4f60;
+        letter-spacing: -.03em;
+    }
+    [class*=col-] p, a, Link {
+        font-size: 14px;
+        margin-bottom: 8px;
+        line-height: 22.08px;
+    }
+    .socials ul {
+        margin-bottom: 5px;
+    }
 
+    .flex--1, .flex--2 {
+        display: -webkit-flex;
+        display: -moz-box;
+        display: flex;
+    }
+    .jc-fe, .jc-lg-fe {
+        -webkit-justify-content: flex-end;
+        -moz-box-pack: end;
+        justify-content: flex-end;
+    }
+    .socials li a {
+        color: #122231;
+        display: inline;
+        margin-bottom: 0;
+    }
+    .socials li {
+        -webkit-transition: .5s;
+        -moz-transition: .5s;
+        transition: .5s;
+        line-height: 0;
+        display: -webkit-flex;
+        display: -moz-box;
+        display: flex;
+        -webkit-align-items: center;
+        -moz-box-align: center;
+        align-items: center;
+        -webkit-justify-content: center;
+        -moz-box-pack: center;
+        justify-content: center;
+        width: 32.8px;
+        height: 32px;
+    }
+    .connect a {
+        line-height: 21px;
+        margin-bottom: 15px;
+    }
+    .text-right {
+        text-align: right;
+    }
+    .about p {
+        font-size: 11px;
+    }
+
+    .about__body, .about__sec-body {
+        max-width: 441px;
+        margin-bottom: 12px;
+        line-height: 22px;
+        color: rgba(61,79,96,.6509803922);
+    }
+    .about__body {
+        max-width: 619px;
+        margin-bottom: 31px;
+    }
+    .primary {
+        color: #083e9e;
+    }
+
+    @media screen and (min-width: 992px){
+        .col-md-3 {
+            -webkit-flex: 0 0 25%;
+            -moz-box-flex: 0;
+            flex: 0 0 25%;
+            max-width: 0 0 25%;
+        }
+        .col-md-2x {
+            -webkit-flex: 0 0 15%;
+            -moz-box-flex: 0;
+            flex: 0 0 15%;
+            max-width: 0 0 15%;
+        }
+        .col-md-2s {
+            -webkit-flex: 0 0 20%;
+            -moz-box-flex: 0;
+            flex: 0 0 20%;
+            max-width: 0 0 20%;
+        }
+        .col-md-3 {
+            -webkit-flex: 0 0 25%;
+            -moz-box-flex: 0;
+            flex: 0 0 25%;
+            max-width: 0 0 25%;
+        }
+
+    }
+    
     @media screen and (max-width: 928px){
         /* padding-bottom: 10px; */
         .faq .accordion__button h3{
@@ -26,7 +179,7 @@ function Footer() {
                             <Link className="logo mb-25" to="/">
                                 <img src={PiggyvestLogo} alt="" width="172.45" height="32"/>
                             </Link>
-                            <img src="" alt="" width="102.14"/>
+                            <img src={Compliance} alt="" width="102.14"/>
                         </div>
                     </div>
                     <div className="col-md-2x products">
@@ -42,7 +195,7 @@ function Footer() {
                         <p>Company</p>
                         <Link to="/about">About</Link>
                         <Link to="/faq">FAQs</Link>
-                        <a target="_blank" href="https://blog.piggyvest.com/">Blog</a>
+                        <a target="_blank" rel="noreferrer" href="https://blog.piggyvest.com/">Blog</a>
                     </div>
                     <div className="col-md-2s legal">
                         <p>Legal</p>
@@ -55,22 +208,27 @@ function Footer() {
                             <ul className="flex--1 jc-fe socials undefined">
                                 <li className="">
                                     <a className="" target="_blank" rel="noreferrer noopener" href="https://www.facebook.com/PiggyBankNG/">
+                                        <img src={facebook}  alt="" />
                                     </a>
                                 </li>
                                 <li className="">
                                     <a className="" target="_blank" rel="noreferrer noopener" href="https://www.instagram.com/piggybankng/">
+                                    <img src={instagram}  alt="" />
                                     </a>
                                 </li>
                                 <li className="">
                                     <a className="" target="_blank" rel="noreferrer noopener" href="https://mobile.twitter.com/PiggyBankNG/">
+                                        <img src={twitter}  alt="" />
                                     </a>
                                 </li>
                                 <li className="">
                                     <a className="" target="_blank" rel="noreferrer noopener" href="https://www.tiktok.com/@piggybank_ng?_t=8cwbLU1TTcs&amp;_r=1">
+                                        <img src={tiktok} alt=""  />
                                     </a>
                                 </li>
                                 <li className="">
                                     <a className="" target="_blank" rel="noreferrer noopener" href="https://www.youtube.com/@PiggyVest">
+                                        <img src={youtube}  alt="" />
                                     </a>
                                 </li>
                             </ul>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { styled } from "styled-components";
 
+const StyledMain = styled.main`
+    .hi:not(:last-child) {
+        margin-bottom: 10rem;
+    }
+`;
 function Home() {
     return (
-        <>
-            <h1>This is the Home page</h1>
-        </>
+        <StyledMain>
+            <h1 className="hi">This is the Home page</h1>
+            <h1 className="hi">This is the Home page</h1>
+            <h1 className="hi">This is the Home page</h1>
+            <h1 className="hi">This is the Home page</h1>
+            <h1 className="hi">This is the Home page</h1>
+        </StyledMain>
     );
 }
 export default Home;
